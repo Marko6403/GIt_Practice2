08e8b1e1782640255ba73dd5aa2acaf460f8ed0b
Marko Savic <tvojemail@gmail.com>
2026-03-08 17:03:35 +0100

    Version 4 ArrayList

---
+++ Data_Structures/src/ArrayLists.java
@@ -0,0 +1,40 @@
+public class ArrayLists {
+
+    int initial_value;
+    int[]niz;
+    int size;
+
+    public ArrayLists(int initial_value)
+    {
+        this.initial_value = initial_value;
+        niz = new int[initial_value];
+        size = 0;
+    }
+
+    public void add(int value)
+    {
+        if(size==niz.length)
+            resize();
+        niz[size] = value;
+        size++;
+    }
+    public int get(int index)
+    {
+        return niz[index];
+    }
+
+
+
+
+    public  void resize()
+    {
+        int[]new_niz = new int[size*2];
+        for(int i = 0;i<niz.length;i++)
+        {
+            new_niz[i] = niz[i];
+        }
+        niz = new_niz;
+
+    }
+
+}
--- Data_Structures/src/Main.java
+++ Data_Structures/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Bro");
+
+
+
+
     }
 }
