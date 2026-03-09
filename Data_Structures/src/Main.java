b83007d041b9feb6ca7a1aa5f32f9d832a15515c
Marko Savic <tvojemail@gmail.com>
2026-03-09 23:38:34 +0100

    Version Graph & DFS

--- Data_Structures/src/Main.java
+++ Data_Structures/src/Main.java
@@ -5,9 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<LinkedList<Integer>> list = new ArrayList<>();
-        Integer b = list.get(0);
-
+      
 
 
 
